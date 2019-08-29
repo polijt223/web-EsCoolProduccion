2af72802ab5540fc098e6a6afd7fbdc5c673d510
polijt223 <pablotulian10@gmail.com>
2019-08-28 21:55:40 -0300

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+
+//Musica
+function colorearImagenMusica(){
+    document.getElementById("imgsonido").setAttribute("src","../img/servicios/dj-720589_1920.jpg");
+}
+function desColorearImagenMusica(){
+    document.getElementById("imgsonido").setAttribute("src","../img/servicios/dj-720589bw_1920.jpg");
+}
+
+
+//Iluminacion
+function colorearImagenIluminacion(){
+    document.getElementById("imgIluminacion").setAttribute("src","../img/servicios/audience-1853662_1920.jpg");
+}
+function desColorearImagenIluminacion(){
+    document.getElementById("imgIluminacion").setAttribute("src","../img/servicios/audiencebw-1853662_1920.jpg");
+}
+
+
+//DJ
+function colorearImagenDj(){
+    document.getElementById("imgDj").setAttribute("src","../img/servicios/dj-2581269_1920.jpg");
+}
+function desColorearImagenDj(){
+    document.getElementById("imgDj").setAttribute("src","../img/servicios/dj-2581269bw_1920.jpg");
+}
+
+
+//FOTOGRAFIA
+function colorearImagenFotografia(){
+    document.getElementById("imgFotografia").setAttribute("src","../img/servicios/night-1927265_1920.jpg");
+}
+function desColorearImagenFotografia(){
+    document.getElementById("imgFotografia").setAttribute("src","../img/servicios/night-bw1927265_1920.jpg");
+}
+
+
+//DISEÑO
+function colorearImagenDiseno(){
+    document.getElementById("imgDiseno").setAttribute("src","../img/servicios/desktop-1985856_1920.jpg");
+}
+function desColorearImagenDiseno(){
+    document.getElementById("imgDiseno").setAttribute("src","../img/servicios/desktopbw-1985856_1920.jpg");
+}
